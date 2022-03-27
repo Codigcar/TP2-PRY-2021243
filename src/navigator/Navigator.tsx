71c6518eddd1c39096e3c8ab0e18e2fc7fafadbe
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2022-03-27 01:16:37 -0400

    Added icons && StackNavigator

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { MapScreen, PermissionsScreen } from '../pages';
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown:false,
+        cardStyle:{
+          backgroundColor:'white'
+        }
+      }}
+    >
+        <Stack.Screen name="MapScreen" component={MapScreen} ></Stack.Screen>
+        <Stack.Screen name="PermissionScreen" component={PermissionsScreen} ></Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+
+export default Navigator
