accfd4391998ec6a661309e1fb2942dfcf8e401e
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2022-03-27 02:18:30 -0400

    [Permisos] Added solicitar permisos con PermissionContext

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react'
-import { PermissionStatus } from 'react-native-permissions';
+import { Platform } from 'react-native';
+import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
 
 export interface PermissionState {
     localStatus: PermissionStatus;
@@ -17,12 +18,23 @@ type PermissionContextProps = {
 
 export const PermissionContext = createContext({} as PermissionContextProps); // TODO: que data compartirá
 
-export const PermissionsProvider = ({ children }: any) => {
+const PermissionsProvider = ({ children }: any) => {
 
     const [permissions, setPermissions] = useState(permissionInitState);
 
-    const askLocationPermission = () => {
+    const askLocationPermission = async() => {
+        let permissionStatus: PermissionStatus;
 
+        if (Platform.OS === 'ios') {
+            permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+        } else {
+            permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+        }
+
+        setPermissions({
+            ...permissions,
+            localStatus: permissionStatus
+        })
     }
     const checkLocationPermission = () => {
 
@@ -38,3 +50,5 @@ export const PermissionsProvider = ({ children }: any) => {
         </PermissionContext.Provider>
     )
 }
+
+export { PermissionsProvider }
--- src/pages/PermissionsScreen.tsx
+++ src/pages/PermissionsScreen.tsx
@@ -1,30 +1,20 @@
-import React from 'react'
-import { Button, LogBox, Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react'
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import { PermissionContext } from '../context/PermissionsContext';
 import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
 
-LogBox.ignoreAllLogs
-LogBox.ignoreLogs([
-  "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
-]);
 
 const PermissionsScreen = () => {
 
-  const checkLocationPermission = async () => {
-    let permissionStatus: PermissionStatus;
-
-    if (Platform.OS === 'ios') {
-      permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-    } else {
-      permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-    }
-    console.log({ permissionStatus });
-
-  }
+  const { permissions, askLocationPermission } = useContext(PermissionContext);
 
   return (
     <View style={styles.container} >
       <Text>Permisos GPS</Text>
-      <Button title='Permiso' onPress={checkLocationPermission} />
+      <Button title='Permiso' onPress={askLocationPermission} />
+      <Text>
+        {JSON.stringify(permissions, null, 5)}
+      </Text>
     </View>
   )
 }
