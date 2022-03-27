7e78b4d68d579f37e00fbaa7447cdbd0bbdd2d57
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2022-03-27 02:41:46 -0400

    [Permisos] Added detectar que pantalla mostrar al inicio de forma automatica

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
-import { MapScreen, PermissionsScreen } from '../pages';
+import { LoadingScreen, MapScreen, PermissionsScreen } from '../pages';
+import { PermissionContext } from '../context/PermissionsContext';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
+
+  const { permissions } = useContext(PermissionContext);
+
+  if (permissions.localStatus === 'unavailable') {
+    return <LoadingScreen />
+  }
+
   return (
     <Stack.Navigator
-    initialRouteName="PermissionScreen"
+      initialRouteName="PermissionScreen"
       screenOptions={{
-        headerShown:false,
-        cardStyle:{
-          backgroundColor:'white'
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white'
         }
       }}
     >
-        <Stack.Screen name="MapScreen" component={MapScreen} ></Stack.Screen>
-        <Stack.Screen name="PermissionScreen" component={PermissionsScreen} ></Stack.Screen>
+      {
+        (permissions.localStatus === 'granted')
+        ? <Stack.Screen name="MapScreen" component={MapScreen} ></Stack.Screen>
+        : <Stack.Screen name="PermissionScreen" component={PermissionsScreen} ></Stack.Screen>
+      }
     </Stack.Navigator>
   )
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export * from './MapScreen';
 export * from './PermissionsScreen';
+export * from './LoadingScreen';
