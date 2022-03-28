38f267b0b9aa6b2a4abd5a09f4e372f145fdb15b
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2022-03-28 02:46:05 -0400

    [Maps] Added google-maps

--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -4,15 +4,16 @@ import MapView from 'react-native-maps';
 
 export const MapScreen = () => {
   return (
-    <View style={{ flex: 1 }}>
       <MapView
+        style={{ flex: 1 }}
+        showsUserLocation
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
-    </View>
+      ></MapView>
+
   )
 }
