437a51f778c06e8ca59558dd79c48e2d8a0638a2
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2022-03-28 04:31:15 -0400

    [TabNavigator] Added material bottom tabs

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -4,9 +4,15 @@ import { LoadingScreen } from '../pages/LoadingScreen';
 import { MapScreen } from '../pages/MapScreen';
 import { PermissionsScreen } from '../pages/PermissionsScreen';
 import { PermissionsContext } from '../context/PermissionsContext';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AccidentsScreen } from '../pages/AccidentsScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Text } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 const Stack = createStackNavigator();
-
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 export const Navigator = () => {
 
   const { permissions } = useContext(PermissionsContext);
@@ -27,9 +33,50 @@ export const Navigator = () => {
     >
       {
         (permissions.locationStatus === 'granted')
-        ? <Stack.Screen name="MapScreen" component={MapScreen} ></Stack.Screen>
-        : <Stack.Screen name="PermissionScreen" component={PermissionsScreen} ></Stack.Screen>
+          ? <Stack.Screen name="MapScreen" component={BottomNavigator} ></Stack.Screen>
+          : <Stack.Screen name="PermissionScreen" component={PermissionsScreen} ></Stack.Screen>
       }
     </Stack.Navigator>
   )
+}
+
+export const BottomNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarActiveTintColor: 'white',
+        tabBarStyle: {
+          borderTopColor: 'red',
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12
+        },
+        tabBarIcon: ({ color, focused }) => {  //icons
+
+          let iconName: string = '';
+
+          switch (route.name) {
+            case 'Home':
+              iconName = 'navigate'
+              break;
+            case 'Accidents':
+              iconName = 'list'
+              break;
+          }
+
+          return <Icon name={iconName} size={20} color={'red'} />
+        }
+      })}
+      barStyle={{
+        backgroundColor: 'white'
+      }}
+      sceneAnimationEnabled={true}
+    >
+      <Tab.Screen name="Home" options={{ title: 'Mapa' }} component={MapScreen} />
+      <Tab.Screen name="Accidents" options={{ title: 'Accidentes' }} component={AccidentsScreen} />
+    </Tab.Navigator>
+  );
 }
