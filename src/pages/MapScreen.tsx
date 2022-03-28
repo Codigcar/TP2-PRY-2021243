289f70f2fc39a6baefdbb5054641e3d0d7a72761
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2022-03-28 01:40:29 -0400

    [Maps] Added ubicacion init

--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 export const MapScreen = () => {
   return (
-      <View>
-          <Text>MapScreen</Text>
-      </View>
+    <View style={{ flex: 1 }}>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
   )
 }
